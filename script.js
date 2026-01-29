0abc461bbea35f4a830b0c44b965c72b53f2bae8
Yilin Liu <yilin.liu@duke.edu>
2026-01-29 08:49:34 -0500

    fix: update button alert message

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const button = document.getElementById('helloBtn');
+
+
+button.addEventListener('click', function() {
+  
+    console.log("Hello world");
+    
+    
+    alert("Check your browser console! (Right click -> Inspect -> Console)");
+});
